5b605004228d6dc99deb3ba932cbb331aff7abec
Dedi Setiawan <dedi.setiawan@primecash.co.id>
2024-06-10 08:01:41 +0700

    create error handler [wip]

--- backend/player/middleware/ErrorHandler.js
+++ backend/player/middleware/ErrorHandler.js
@@ -1,15 +1,7 @@
 const mongoose = require('mongoose');
 
-function errorHandler(err, req, res, next) {
-    if (res.headersSent) {
-        return next(err);
-    }
-
-    if (err instanceof mongoose.Error.ValidationError) {
-        return res.status(400).send({ message: err.message });
-    }
-
-    res.status(500).send({ message: 'Internal Server Error' });
+function errorHandler(err, _req, res) {
+    res.status(err.httpCode).send({ message: err.message });
 }
 
 module.exports = errorHandler;
