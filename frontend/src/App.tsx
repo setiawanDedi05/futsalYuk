452fcabe4a87cb27b813eae37873111f4177217b
Dedi Setiawan <dedi.setiawan@primecash.co.id>
2024-06-24 23:20:45 +0700

    add frontend animate hero image

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
-import React, { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
 import Picture from "./assets/images/Green Soccer Field Aerial.jpg";
+import { Reveal } from "./uikit/reveal";
+import Lenis from 'lenis'
+
+const lenis = new Lenis();
+
 export default function App() {
   const container = useRef(null);
   const { scrollYProgress } = useScroll({
@@ -8,16 +13,41 @@ export default function App() {
     offset: ['start start', 'end end']
   })
 
-  const scale4 = useTransform(scrollYProgress, [0, 1], [1, 4])
+  useEffect(() => {
+    function raf(time: any) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+  }, [])
+
+  const scale4 = useTransform(scrollYProgress, [0, 0.5], [5, 0.5])
   return (
-    <div ref={container} className="container">
-      <div className="sticky">
-        <motion.div style={{scale: scale4}} className="el">
-          <div className="image-container">
-            <img className="image-item" src={Picture} />
+    <>
+      <div ref={container} className="container">
+        <div className="sticky">
+          <div className="backdrop">
+            <Reveal>
+              <h1 className="headTitle block">GoRivalGo</h1>
+            </Reveal>
+            <Reveal>
+              <h2 className="block">
+                Cari Teman Futsal mu dan Ayo Olahraga!
+              </h2>
+            </Reveal>
           </div>
-        </motion.div>
+          <motion.div style={{ scale: scale4 }} className="el">
+            <div className="image-container">
+              <img className="image-item" src={Picture} alt="image-hero" />
+            </div>
+          </motion.div>
+        </div>
       </div>
-    </div>
+      <div style={{ position: "absolute", top: "150vh", zIndex: 100 }}>
+        <div style={{ color: "white" }}>Register</div>
+        <div style={{ color: "white" }}>Login</div>
+      </div>
+    </>
   );
 }
---
+++ frontend/src/uikit/reveal.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useRef } from "react";
+import { motion, useAnimation, useScroll } from "framer-motion";
+
+interface Props {
+    children: JSX.Element;
+    width?: "fix-content" | "100%";
+}
+
+export const Reveal = ({ children, width = "fix-content" }: Props) => {
+    const ref = useRef(null)
+    const { scrollY } = useScroll({
+        target: ref
+    });
+
+    const mainControls = useAnimation();
+    const slideControls = useAnimation();
+
+    useEffect(() => {
+        if (scrollY) {
+            mainControls.start("visible");
+            slideControls.start("visible");
+        }
+    }, [mainControls, slideControls, scrollY])
+    return (
+        <div ref={ref} style={{ position: "relative", width, overflow: "hidden" }}>
+            <motion.div
+                variants={{
+                    hidden: { opacity: 0, y: 75 },
+                    visible: { opacity: 1, y: 0 }
+                }}
+                initial="hidden"
+                animate={mainControls}
+                transition={{ duration: 0.5, delay: 0.25 }}
+            >{children}</motion.div>
+            <motion.div
+                variants={{
+                    hidden: { left: 0 },
+                    visible: { left: "100%" }
+                }}
+                initial="hidden"
+                animate={slideControls}
+                transition={{
+                    duration: 0.5,
+                    ease: "easeIn"
+                }}
+                style={{
+                    position: "absolute",
+                    top: 4,
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    backgroundColor: "rgba(255,0,0,1)",
+                    zIndex: 5,
+                }}
+            />
+        </div>
+    );
+}
