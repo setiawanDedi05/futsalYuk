ff50968ada55d00ba407681cad4e3afb6891ffcc
setiawanDedi05 <dssetiawan05@gmail.com>
2024-08-13 21:58:57 +0700

    reformating mongoose connection

--- backend/post/helpers/mongooseConnection.js
+++ backend/post/helpers/mongooseConnection.js
@@ -1,13 +1,25 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-const clientOptions = { serverApi: { socketOptions: { connectTimeoutMS: 1000 }, version: '1', strict: true, deprecationErrors: true } };
+require("dotenv").config();
+const mongoose = require("mongoose");
+const clientOptions = {
+  serverApi: {
+    socketOptions: { connectTimeoutMS: 1000 },
+    version: "1",
+    strict: true,
+    deprecationErrors: true,
+  },
+};
 
 async function dbconnect() {
-    await mongoose.connect(process.env.NODE_ENV === 'test' ? `${process.env.MONGO_URL}-test` : process.env.MONGO_URL, clientOptions)
+  await mongoose.connect(
+    process.env.NODE_ENV === "test"
+      ? `${process.env.MONGO_URL}-test`
+      : process.env.MONGO_URL,
+    clientOptions
+  );
 }
 
 function dbclose() {
-    return mongoose.disconnect();
+  return mongoose.disconnect();
 }
 
 module.exports = { dbconnect, dbclose };
