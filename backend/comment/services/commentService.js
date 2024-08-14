99185511012a28d6a9a8797beaf57124ffdfc459
setiawanDedi05 <dssetiawan05@gmail.com>
2024-08-14 21:33:05 +0700

    removing hardcode commentId in comment service

--- backend/comment/services/commentService.js
+++ backend/comment/services/commentService.js
@@ -6,25 +6,30 @@ class CommentService {
   }
 
   async create(comment) {
-    amqp.connect(process.env.rabbit_MR_URL, (error0, connection) => {
-      if (error0) throw error0;
+    try {
+      const response = await this.commentRepository.create(comment);
+      amqp.connect(process.env.rabbit_MR_URL, (error0, connection) => {
+        if (error0) throw error0;
 
-      connection.createChannel((error1, channel) => {
-        if (error1) throw error1;
+        connection.createChannel((error1, channel) => {
+          if (error1) throw error1;
 
-        const queue = "create_comment";
-        const msg = JSON.stringify({
-          postId: comment.postId,
-          commentId: "1", //ini ganti dengan comment id
+          const queue = "create_comment";
+          const msg = JSON.stringify({
+            postId: comment.postId,
+            commentId: response._id, //ini ganti dengan comment id
+          });
+
+          channel.assertQueue(queue, { durable: false });
+          channel.sendToQueue(queue, Buffer.from(msg));
+
+          console.log("[x] Sent %s", msg);
         });
-
-        channel.assertQueue(queue, { durable: false });
-        channel.sendToQueue(queue, Buffer.from(msg));
-
-        console.log("[x] Sent %s", msg)
       });
-    });
-    return await this.commentRepository.create(comment);
+      return response;
+    } catch (error) {
+      throw(error)
+    }
   }
 }
 
