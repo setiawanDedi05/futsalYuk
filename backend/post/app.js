79dc74fc90d0b23e095ec1049a657aee0cdbbfb2
setiawanDedi05 <dssetiawan05@gmail.com>
2024-08-14 22:14:21 +0700

    add event consumer in

--- backend/post/app.js
+++ backend/post/app.js
@@ -1,11 +1,14 @@
 const express = require("express")
 const postRouter = require("./routes/postRoute");
 const errorHandlerMiddleware = require("./middleware/ErrorHandler");
+const { startMessageHandler } = require("./events/messageHandler");
 const app = express()
 
 app.use(express.json());
 
 app.use("/", postRouter); 
+
+startMessageHandler();
 
 app.use(errorHandlerMiddleware)
 
