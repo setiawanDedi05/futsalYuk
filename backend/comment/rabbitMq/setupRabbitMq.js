dc1c8dca4defbc0ff916baeae777669fb3e58a3b
setiawanDedi05 <dssetiawan05@gmail.com>
2024-08-21 23:48:44 +0700

    reformating and change key comment.delete

--- backend/comment/rabbitMq/setupRabbitMq.js
+++ backend/comment/rabbitMq/setupRabbitMq.js
@@ -1,35 +1,44 @@
 const amqp = require("amqplib/callback_api");
 
 const publishCommentCreated = (comment) => {
-    amqp.connect(process.env.rabbit_MR_URL, (error0, connection)=> {
-        if(error0) throw error0;
+  amqp.connect(process.env.rabbit_MR_URL, (error0, connection) => {
+    if (error0) throw error0;
 
-        connection.createChannel((error1, channel) => {
-            if(error1) throw error1;
+    connection.createChannel((error1, channel) => {
+      if (error1) throw error1;
 
-            const exchange = "comment_topic_exchange";
+      const exchange = "comment_topic_exchange";
 
-            channel.publish(exchange, "comment.created", Buffer.from(JSON.stringify(comment)));
+      channel.publish(
+        exchange,
+        "comment.created",
+        Buffer.from(JSON.stringify(comment))
+      );
 
-            channel.close(() => connection.close());
-        });
+      channel.close(() => connection.close());
     });
+  });
 };
 
 const publishCommentDeleted = (comment) => {
-    amqp.connect(process.env.rabbit_MR_URL, (error0, connection)=> {
-        if(error0) throw error0;
+  amqp.connect(process.env.rabbit_MR_URL, (error0, connection) => {
+    if (error0) throw error0;
 
-        connection.createChannel((error1, channel) => {
-            if(error1) throw error1;
+    connection.createChannel((error1, channel) => {
+      if (error1) throw error1;
 
-            const exchange = "comment_topic_exchange";
+      const exchange = "comment_topic_exchange";
 
-            channel.publish(exchange, "comment.created", Buffer.from(JSON.stringify(comment)));
+      channel.publish(
+        exchange,
+        "comment.deleted",
+        Buffer.from(JSON.stringify(comment))
+      );
 
-            channel.close(() => connection.close());
-        });
+      channel.close(() => connection.close());
     });
+  });
 };
 
-module.exports = { publishCommentCreated, publishCommentDeleted }
+module.exports = { publishCommentCreated, publishCommentDeleted };
+
